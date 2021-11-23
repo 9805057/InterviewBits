f2486cec9a43cb07cca044aa9ea77744c223a582
9805057 <94931739+9805057@users.noreply.github.com>
2021-11-23 12:58:40 -0800

    Update pick-from-both-sides.cpp

--- Arrays/pick-from-both-sides.cpp
+++ Arrays/pick-from-both-sides.cpp
@@ -1,1 +1,19 @@
+// 
+// https://www.interviewbit.com/problems/pick-from-both-sides/
+//
 
+int Solution::solve(vector<int> &A, int B) {
+    int N=A.size();
+    int sum = 0;
+    for(int i=0; i<B; i++) {
+        sum += A[i];
+    }
+
+    int max_value = sum;
+    for(int i=0; i<B; i++) {
+        sum = sum - A[B-1-i] + A[N-1-i];
+        max_value = std::max(max_value, sum); 
+    }
+    
+    return max_value;
+}
